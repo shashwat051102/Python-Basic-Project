d0dc08793425994434ebf461862d9e9db6b82fed
shashwat051102 <137853743+shashwat051102@users.noreply.github.com>
2023-07-11 23:23:53 +0530

    Add files via upload

---
+++ Bubble sort.py
@@ -0,0 +1,49 @@
+
+# li = [3, 2, 7, 1, 8]
+
+# for i in range(len(li)):
+    
+#     swapped = False
+    
+    
+#     for j in range(0, len(li)-i):
+        
+#         if li[j] > li[j*1]:
+#             li[j], li[j+1] = li[j+1], li[j]
+#             swapped = True
+            
+#         if swapped == False:
+#             break
+        
+#     print(li)
+
+
+def bubbleSort(arr):
+    n = len(arr)
+      
+    # Traverse through all array elements
+    for i in range(n):
+        swapped = False
+  
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
+  
+            # Traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if (swapped == False):
+            break
+  
+  
+# Driver code to test above
+if __name__ == "__main__":
+    arr = [64, 34, 25, 12, 22, 11, 90]
+  
+    bubbleSort(arr)
+  
+    print("Sorted array:")
+    for i in range(len(arr)):
+        print("%d" % arr[i], end=" ")
